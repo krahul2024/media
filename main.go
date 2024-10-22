19011fb6d3676c31804385c46b98f29a8b681934
rahul <sandipkumar2024@gmail.com>
2024-10-22 21:06:26 +0530

    update some stuff

--- main.go
+++ main.go
@@ -9,15 +9,13 @@ import (
 	"github.com/krahul2024/go-multer/utils"
 )
 
+func main() {
+	utils.SetEnv()
+	PORT := os.Getenv("PORT")
 
-func main(){
-    utils.SetEnv()
-    PORT := os.Getenv("PORT")
+	router := routes.NewRouter()
+	routes.Register(router)
 
-    router := routes.NewRouter();    
-    routes.Register(router);
-
-    fmt.Println(fmt.Sprintf("Server runinng on the port: %v", PORT))
-    http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil)
-} 
-
+	fmt.Println(fmt.Sprintf("Server runinng on the port: %v", PORT))
+	http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil)
+}
